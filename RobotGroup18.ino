2411300b2deceb2b7a1c1fcf04954f1b6781b8eb
Anas <anas@asherif.xyz>
2024-05-27 15:42:42 -0400

    Update RobotGroup18.ino
    Added variables to track time for IR sensor and added a check that reads the IR sensor every 500ms

--- RobotGroup18.ino
+++ RobotGroup18.ino
@@ -11,10 +11,19 @@ void setup() {
   pinMode(IR_2, INPUT);
   pinMode(IR_3, INPUT);
   pinMode(IR_4, INPUT);
-
 }
+
+unsigned long irSensorMillis = 0; // timer to track the last report of the IR sensors
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  // get the current run time in milliseconds
+  unsigned long currentMillis millis();
 
+  // check the states of the IR sensors every 500ms
+  if (currentMillis — irSensorMillis >= 500) {
+    irSensorMillis = currentMillis;
+    readInfrared();
+  }
 }
