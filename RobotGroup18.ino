8a347ced3d0dc7ea48fc3797289fa383490a456b
Anas <anas@asherif.xyz>
2024-05-27 15:57:16 -0400

    Added Color sensor Logic and buxfixes.
    Added Color sensor Logic.
    Bugfixes, so now it compiles.
    Added readColorSensor() method which reads the color sensor then determines a color by the average of the colors that are read.
    Added if statement that reads the color sensor every 250ms.
    Added color sensor library.

--- RobotGroup18.ino
+++ RobotGroup18.ino
@@ -1,3 +1,11 @@
+#include <tcs3200.h>
+
+#define COLOR_0 A0
+#define COLOR_1 A1
+#define COLOR_2 A2
+#define COLOR_3 A3
+#define COLOR_OUT A4
+
 #define IR_1 4
 #define IR_2 7
 #define IR_3 8
@@ -14,16 +22,23 @@ void setup() {
 }
 
 unsigned long irSensorMillis = 0; // timer to track the last report of the IR sensors
+unsigned long colorSensorMillis = 0; // timer to track the last report of the color sensors
 
 void loop() {
   // put your main code here, to run repeatedly:
   
   // get the current run time in milliseconds
-  unsigned long currentMillis millis();
+  unsigned long currentMillis = millis();
 
   // check the states of the IR sensors every 500ms
-  if (currentMillis — irSensorMillis >= 500) {
+  if (currentMillis - irSensorMillis >= 500) {
     irSensorMillis = currentMillis;
     readInfrared();
+  }
+
+  // read the color sensor
+  if (currentMillis - colorSensorMillis >= 250) {
+    colorSensorMillis = currentMillis;
+    readColorSensor();
   }
 }
